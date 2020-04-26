df08c69756acf53c058db4f4dbe3b34b2fc3ca29
JBrenneman444 <JBrenneman444@gmail.com>
2020-04-26 13:01:10 -0700

    Commit 8: Can POST new Tweets.

--- wk20/tweeter_app_api/app/controllers/tweets_controller.rb
+++ wk20/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -10,4 +10,21 @@ class TweetsController < ApplicationController
         render json: {status: 200, tweet: tweet}
     end
 
+    def create
+        tweet = Tweet.new(tweet_params)
+    
+        if tweet.save
+          render(json: { tweet: tweet }, status: 201)
+        else
+          # Unprocessable Entity
+          render(json: { tweet: tweet }, status: 422)
+        end
+    end
+  
+    private # Any methods below here
+  
+    def tweet_params
+      # Returns a sanitized hash of the params with nothing extra
+      params.required(:tweet).permit(:title, :content, :author)
+    end
 end
