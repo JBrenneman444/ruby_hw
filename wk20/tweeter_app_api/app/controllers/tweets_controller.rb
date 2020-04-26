d8ba17e70e9801bed54fac18264c45d199e11dfc
JBrenneman444 <JBrenneman444@gmail.com>
2020-04-26 12:48:36 -0700

    Commit 6: All Tweets are visible at the index route.

--- wk20/tweeter_app_api/app/controllers/tweets_controller.rb
+++ wk20/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -1,9 +1,12 @@
-class Tweets < ApplicationController
+class TweetsController < ApplicationController
     
     def index
+        tweets = Tweet.all
+        render json: {status: 200, tweets: tweets}
     end
 
     def show
+        
     end
 
 end
